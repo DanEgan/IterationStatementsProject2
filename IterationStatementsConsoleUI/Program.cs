ca3a6208931d4cd8240e91c6af0dadea65611157
DanEgan <you@example.com>
2020-06-23 16:07:16 -0400

    second try

---
+++ IterationStatementsConsoleUI/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Globalization;
+
+namespace IterationStatementsConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////// WHILE ///////////////////////////////////
+
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine("Hello User!");
+            //    i++;
+            //}
+
+            /////////////////////// DO-WHILE ///////////////////////////////////
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Hello User!");
+                i++;
+            } while (i < 10);
+
+            /////////////////////////   FOR LOOP    //////////////////////////////////
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Hello User!");
+            //}
+
+            ///////////////////////////   FOR LOOP ARRAY - ITERATE THROUGH AN ARRAY    //////////////////////////////////
+            //int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    //Console.WriteLine($"{numbers[i]} is at the {i} index in the array");
+            //    numbers[i] += 1;
+            //    Console.WriteLine($"{numbers[i]} is at the {i} index in the array");
+
+            //}
+
+            ///////////////////////// FOREACH ///////////////////////////////////
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+            //foreach (int number in numbers)
+            //{
+
+            //    Console.WriteLine($"{number}");
+
+            //}
+
+            ////Array Example
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+            ////List Example
+            //List<int> numberList = new List<int>();
+
+            //List<int> numberList2 = { 1, 2, 3, 4, 5, 6 };
+
+            //numberList.Add(1);
+            //numberList.Add(2);
+            //numberList.Add(3);
+            //numberList.Add(4);
+            //numberList.Add(5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
